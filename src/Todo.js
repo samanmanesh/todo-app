e7bd0f0514cd783c1265e6af4f7966faee2ade40
Saman Manesh <samanmanesh7@gmail.com>
2021-07-23 20:43:17 -0400

    about to changed the layout styles

--- src/Todo.js
+++ src/Todo.js
@@ -26,33 +26,44 @@ export default function Todo({
 
   //   return dayjs(todoData.reminder).format('YYYY-MM-DD');
 
-   
   //   // return day.isBefore(dayjs())
   //   //   ? dayjs(todoData.reminder).format("YYYY-MM-DD")
   //   //   : "";
   // };
   const displayDue = () => {
     if (todoData.due) return dayjs(todoData.due).format(" MMM-DD");
-  }
+  };
+  const displayDot = () => {
+    if (todoData.reminder && todoData.due) return " . ";
+  };
   return (
     <div className="todo-elements">
       <input
+        className="checkbox"
         type="checkbox"
         checked={todoData.complete}
         onChange={handleTodoClick}
       />
       <div onClick={() => openEditBar(todoData.id)}>
-        <span>{todoData.name}</span>
-          {todoData.due && 
-            <div className="todo-due"> <Calendar width={13} height={13}/>
+        <span className="todo-name">
+          {todoData.name}
+          <div className="option-container">
+            {todoData.due && (
+              <div className="todo-due">
+                <Calendar
+                  className="todo-due-calendar"
+                  width={13}
+                  height={13}
+                />
                 {displayDue()}
-            </div>  
-          } 
-          
-        {todoData.reminder &&
-          <ReminderLogo className="reminder-logo" width={13} height={13}/>
-        }
-        
+              </div>
+            )}
+            <span className="dot">{displayDot()}</span>
+            {todoData.reminder && (
+              <ReminderLogo className="reminder-logo" width={13} height={13} />
+            )}
+          </div>
+        </span>
       </div>
       <button onClick={handleImportant}>
         <Important className="logo" width={14} height={14} />
